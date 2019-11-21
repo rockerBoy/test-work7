9a12cdf2e4afd687188c76da9470934337d3030e
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-11-21 20:20:52 +0500

    Сортировка задач

--- app/src/HTTP/ShowTask.php
+++ app/src/HTTP/ShowTask.php
@@ -20,6 +20,18 @@ class ShowTask implements RequestHandlerInterface
      * @var Connection
      */
     private $connection;
+    /**
+     * @var int
+     */
+    private $page;
+    /**
+     * @var string
+     */
+    private $sort;
+    /**
+     * @var string
+     */
+    private $order;
 
     public function __construct(Environment $twig, Connection $connection)
     {
@@ -29,19 +41,49 @@ class ShowTask implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $body = $request->getQueryParams();
-        $page = $body['page'] ?? 1;
+        $query = $request->getQueryParams();
+        $builder = $this->connection->createQueryBuilder()->from('tasks');
+        $builder->select('*');
+        $whishListSorts = ['username', 'email', 'status'];
+        $this->sort = '';
+        $this->order = 'asc';
+        if (!empty($query['sort']) && in_array($query['sort'], $whishListSorts)) {
+            if (!empty($query['order']) && ($query['order'] === 'asc' || $query['order'] === 'desc')) {
+                $this->order = $query['order'];
+            }
+            $builder->orderBy($query['sort'], $this->order);
+            $this->sort = $query['sort'];
+        }
+        $this->page = $query['page'] ?? 1;
         $inPage = 3;
         $count = $this->connection->fetchColumn('SELECT count(*) from tasks');
-        $from = $inPage * ($page - 1);
+        $from = $inPage * ($this->page - 1);
+        $builder->setFirstResult($from);
+        $builder->setMaxResults($inPage);
         return new HtmlResponse($this->twig->render('showTask.twig', [
-            'tasks' => $this->connection->fetchAll("SELECT * FROM tasks LIMIT $from, $inPage"),
+            'tasks' => $this->connection->fetchAll($builder->getSQL(), $builder->getParameters()),
             'pager' => [
-                'current' => $page,
+                'current' => $this->page,
                 'hasNext' => $count > $from + $inPage,
-                'hasPrev' => $page > 1
+                'hasPrev' => $this->page > 1
+            ],
+            'sort' => $this->sort,
+            'order' => $this->order,
+            'sorts' => [
+                'username' => $this->getUrlOrder('username'),
+                'email' => $this->getUrlOrder('email'),
+                'status' => $this->getUrlOrder('status')
             ],
             'username' => $request->getAttribute('username')
         ]));
+    }
+
+    private function getUrlOrder(string $target): string
+    {
+        $url = "?page={$this->page}&sort=$target";
+        if ($this->sort === $target && $this->order === 'asc') {
+            $url .= '&order=desc';
+        }
+        return $url;
     }
 }
